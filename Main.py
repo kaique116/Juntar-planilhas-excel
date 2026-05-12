3c576c1ba5eae75e5c70615fcaf15a1d32f6e98c
Kaique Santos Passos Barreto <poyimog583furnato.com@gmail.com>
2026-05-12 14:13:33 -0300

    Juntar planilhas
    Script para juntar planilhas excel

---
+++ Main.py
@@ -0,0 +1,22 @@
+import tkinter as tk
+import pandas as pd
+
+def juntar_planilhas():
+    planilha1 = pd.read_excel(informar_planilha1.get())
+    planilha2 = pd.read_excel(informar_planilha2.get())
+
+    planilha_completa = pd.concat([planilha1, planilha2], ignore_index=True)
+
+    
+
+janela = tk.Tk()
+janela.title("Juntar Planilhas")
+janela.geometry("300x200")
+
+informar_planilha1 = tk.Entry(janela)
+informar_planilha1.pack()
+informar_planilha2 = tk.Entry(janela)
+informar_planilha2.pack()
+
+botao_juntar = tk.Button(janela, text='Clica aqui para juntar as planilhas', command=juntar_planilhas)
+botao_juntar.pack()
